498a6c15c1b62c295d009673af7beea4f43e6db3
John Pham <john@highlight.run>
2021-09-08 14:58:49 -0700

    Remove compiled files

--- src/components/ReportDialog/ReportDialog.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-var ReportDialog = function (props) {
-    console.log(props);
-    return _jsx("div", {}, void 0);
-};
-export default ReportDialog;
