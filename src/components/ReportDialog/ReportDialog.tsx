1df4cce178034e49e01f607a7a9803c44851e71c
Vadim Korolik <Vadman97@users.noreply.github.com>
2022-04-18 16:36:31 -0700

    [HIG-2172] ReportDialog: improve submission message (#16)
    Show a subtitle and provide a transition for submitting the
    feedback modal.

--- src/components/ReportDialog/ReportDialog.tsx
+++ src/components/ReportDialog/ReportDialog.tsx
@@ -15,6 +15,7 @@ export interface ReportDialogOptions {
   labelClose?: string;
   labelSubmit?: string;
   successMessage?: string;
+  successSubtitle?: string;
   hideHighlightBranding?: boolean;
 }
 
@@ -32,6 +33,7 @@ const ReportDialog = ({
   subtitle2 = "If you’d like to help, tell us what happened below.",
   subtitle = "Our team has been notified.",
   successMessage = "Your feedback has been sent. Thank you!",
+  successSubtitle = "Thank you for sending us feedback. If you have any other concerns/questions, reach out to this application's support email.",
   title = "It looks like we’re having issues.",
   user,
   onCloseHandler,
@@ -41,12 +43,14 @@ const ReportDialog = ({
   const [name, setName] = useState(user?.name || "");
   const [email, setEmail] = useState(user?.email || "");
   const [verbatim, setVerbatim] = useState("");
+  const [sendingReport, setSendingReport] = useState(false);
   const [sentReport, setSentReport] = useState(false);
   // We want to record when the feedback started, not when the feedback is submitted. If we record the latter, the timing could be way off.
   const reportDialogOpenTime = useRef(new Date().toISOString());
 
   const handleSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
+    setSendingReport(true);
 
     if (window?.H?.addSessionFeedback) {
       window.H.addSessionFeedback({
@@ -60,105 +64,122 @@ const ReportDialog = ({
         "Highlight is not initialized. Make sure highlight.run is installed and running."
       );
     }
-    setSentReport(true);
-    if (onSubmitHandler) {
-      onSubmitHandler();
-    }
+    // fake a loading state because addSessionFeedback is async
+    new Promise((r) => window.setTimeout(r, 300)).then(() => {
+      setSendingReport(false);
+      setSentReport(true);
+      if (onSubmitHandler) {
+        onSubmitHandler();
+      }
+    });
   };
 
   return (
     <main className={styles.container}>
       <div className={styles.card}>
-        {!sentReport ? (
-          <>
-            <div>
-              <h1 className={styles.title}>{title}</h1>
-              <h2 className={styles.subtitle}>
-                {subtitle} {subtitle2}
-              </h2>
-            </div>
-            <form className={styles.form} onSubmit={handleSubmit}>
-              <label>
-                {labelName}
-                <input
-                  type="text"
-                  value={name}
-                  name="name"
-                  autoFocus
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                />
-              </label>
-
-              <label>
-                {labelEmail}
-                <input
-                  type="email"
-                  value={email}
-                  name="email"
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-              </label>
-
-              <label>
-                {labelComments}
-                <textarea
-                  value={verbatim}
-                  placeholder="I typed in a name then clicked the button"
-                  name="verbatim"
-                  rows={3}
-                  onChange={(e) => {
-                    setVerbatim(e.target.value);
-                  }}
-                ></textarea>
-              </label>
-
-              <div className={styles.formFooter}>
-                <div className={styles.formActionsContainer}>
-                  <button type="submit" className={styles.button}>
-                    {labelSubmit}
-                  </button>
-                  <button
-                    className={`${styles.button} ${styles.closeButton}`}
-                    onClick={onCloseHandler}
-                    type="button"
-                  >
-                    {labelClose}
-                  </button>
-                </div>
-                {!hideHighlightBranding && (
-                  <div className={styles.ad}>
-                    <p className={styles.logoContainer}>
-                      Crash reports powered by:
-                      {/*  eslint-disable-next-line react/jsx-no-target-blank */}
-                      <a href="https://highlight.run" target="_blank">
-                        <img
-                          src="https://app.highlight.run/logo-24x130.png"
-                          alt="Highlight"
-                          height="25"
-                          className={styles.logo}
-                        />
-                      </a>
-                    </p>
-                  </div>
-                )}
-              </div>
-            </form>
-          </>
-        ) : (
+        <div
+          className={`${styles.cardContents} ${
+            sentReport && styles.cardContentsVisible
+          }`}
+        >
+          <h1 className={styles.title}>{successMessage}</h1>
+          <h4 className={styles.subtitle}>{successSubtitle}</h4>
+          <button
+            className={`${styles.button} ${styles.confirmationButton}`}
+            onClick={onCloseHandler}
+          >
+            Close
+          </button>
+        </div>
+        <div
+          className={`${styles.cardContents} ${
+            !sentReport && styles.cardContentsVisible
+          }`}
+        >
           <div>
-            <h1 className={styles.title}>{successMessage}</h1>
-            <button
-              className={`${styles.button} ${styles.confirmationButton}`}
-              onClick={onCloseHandler}
-            >
-              Close
-            </button>
+            <h1 className={styles.title}>{title}</h1>
+            <h2 className={styles.subtitle}>
+              {subtitle} {subtitle2}
+            </h2>
           </div>
-        )}
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <label>
+              {labelName}
+              <input
+                type="text"
+                value={name}
+                name="name"
+                autoFocus
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </label>
+
+            <label>
+              {labelEmail}
+              <input
+                type="email"
+                value={email}
+                name="email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </label>
+
+            <label>
+              {labelComments}
+              <textarea
+                value={verbatim}
+                placeholder="I typed in a name then clicked the button"
+                name="verbatim"
+                rows={3}
+                onChange={(e) => {
+                  setVerbatim(e.target.value);
+                }}
+              ></textarea>
+            </label>
+
+            <div className={styles.formFooter}>
+              <div className={styles.formActionsContainer}>
+                <button
+                  type="submit"
+                  className={
+                    sendingReport
+                      ? `${styles.button} ${styles.loadingButton}`
+                      : styles.button
+                  }
+                >
+                  {labelSubmit}
+                </button>
+                <button
+                  className={`${styles.button} ${styles.closeButton}`}
+                  onClick={onCloseHandler}
+                  type="button"
+                >
+                  {labelClose}
+                </button>
+              </div>
+              {!hideHighlightBranding && (
+                <div className={styles.ad}>
+                  <p className={styles.logoContainer}>
+                    Crash reports powered by:
+                    {/*  eslint-disable-next-line react/jsx-no-target-blank */}
+                    <a href="https://highlight.run" target="_blank">
+                      <img
+                        src="https://app.highlight.run/logo-24x130.png"
+                        alt="Highlight"
+                        height="25"
+                        className={styles.logo}
+                      />
+                    </a>
+                  </p>
+                </div>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
     </main>
   );
